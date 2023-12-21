9dd1b52ba5971f6efeebede483160b8b0d248975
Mini <89437550+KizunaYuuki@users.noreply.github.com>
2023-12-21 00:39:35 +0000

    Homes App Tutorial

---
+++ myApp/src/app/housing.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+
+  url = 'https://ubiquitous-happiness-qg4gppp9vpwc4xx4-3000.app.github.dev/locations';
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
+  }
+
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(firstName, lastName, email);
+  }
+}
